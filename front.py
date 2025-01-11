b3c5933ee8b2074b75e1d0d7d8248f9004b470ef
Maxryb2005@gmail.com <dinozavrik.tata@gmail.com>
2025-01-11 17:42:17 +0300

    Add a from for text

--- front.py
+++ front.py
@@ -1,19 +1,9 @@
 import Back
 from Back import dis_calc
 
-# Проверка на существование файла Back.py
-try:
-    file = open("Back.py", encoding='utf-8')
-    s = file.readlines()
-    print("Файл существует")
-    file.close()
+a = float(input("Введите первый аргумент: "))
+b = float(input("Введите второй аргумент: "))
+c = float(input("Введите третий аргумент: "))
 
-    a = int(input("Введите первый аргумент: "))
-    b = int(input("Введите второй аргумент: "))
-    c = int(input("Введите третий аргумент: "))
-
-    d = dis_calc(a, b, c)
-    print(d)
-
-except FileNotFoundError:
-    print("Невозможно открыть файл")
+d = dis_calc(a, b, c)
+print("Ответ: ", d)
