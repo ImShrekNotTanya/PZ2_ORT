dc2d395cd5732f48ddc4317639d0547b4b2431ae
Maxryb2005@gmail.com <dinozavrik.tata@gmail.com>
2025-01-11 17:43:36 +0300

    Add a new functions for back

--- back.py
+++ back.py
@@ -1,13 +1,13 @@
 def dis_calc (a, b, c):
 
-    di = ((b**2) - (4 * a * c))
+    di = ((b**2) - (4 * a * c))**0.5
 
     if di > 0:
-        x = (- b + di**0.5) / (2 * a)
-        y = (- b - di**0.5) / (2 * a)
+        x = (- b + di) / (2 * a)
+        y = (- b - di) / (2 * a)
         return x, y
     if di == 0:
         x = - b / 2 * a
         return x
-    if di < 0:
+    else:
         return 0
